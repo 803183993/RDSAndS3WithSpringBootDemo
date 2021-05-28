6748dbe7567a5bb01d69afc0bd90943b8996d27c
amdad.choudhury <amdad.choudhury@outlook.com>
2021-05-28 08:53:09 +0100

    Fixed startUp.sh script.

--- startUp.sh
+++
@@ -1,1 +0,0 @@
-java -jar RdsWithS3Demo-0.0.1-SNAPSHOT.jar --hostname localhost --jdbcUrl jdbc:postgresql://localhost:5432/moviedb --dbUsername app1 --dbPassword password
---
+++ startup.sh
@@ -0,0 +1,1 @@
+java -jar target/RDSAndS3WithSpringBootDemo-0.0.1-SNAPSHOT.jar --hostname localhost --jdbcUrl jdbc:postgresql://localhost:5432/moviedb --dbUsername app1 --dbPassword password
